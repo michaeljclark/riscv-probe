f905693d650fdd64de7872bf3add2ada545e062d
Michael Clark <mjc@sifive.com>
2018-09-04 17:57:17 +1200

    Add program that hangs to test debugger

---
+++ examples/hang/hang.c
@@ -0,0 +1,24 @@
+#include "femto.h"
+
+
+int main()
+{
+	volatile int a;
+	int b, c, d, e, f, g, h, i, j, k, l, m;
+	printf("hang\n");
+	for (;;) {
+		a = b;
+		b = c;
+		c = d;
+		d = e;
+		e = f;
+		f = g;
+		g = h;
+		h = i;
+		i = j;
+		j = k;
+		k = l;
+		l = m;
+		m = a;
+	}
+}
---
+++ examples/hang/rules.mk
@@ -0,0 +1,1 @@
+hang_objs = hang.o
