f8ce5a5459ed8bdc7ae77253138398afbf5028b5
Michael Clark <mjc@sifive.com>
2018-09-08 15:47:43 +1200

    Remove macros and simplify HTIF code

--- libio/io_spike_htif.c
+++ libio/io_spike_htif.c
@@ -4,92 +4,64 @@
 #include "encoding.h"
 #include "femto.h"
 
-# define TOHOST_CMD(dev, cmd, payload) \
-  (((uint64_t)(dev) << 56) | ((uint64_t)(cmd) << 48) | (uint64_t)(payload))
-
-#define FROMHOST_DEV(fromhost_value) ((uint64_t)(fromhost_value) >> 56)
-#define FROMHOST_CMD(fromhost_value) ((uint64_t)(fromhost_value) << 8 >> 56)
-#define FROMHOST_DATA(fromhost_value) ((uint64_t)(fromhost_value) << 16 >> 16)
-
 volatile uint64_t tohost __attribute__((section(".htif")));
 volatile uint64_t fromhost __attribute__((section(".htif")));
 
-volatile int htif_console_buf;
-
 static spinlock_t htif_lock = SPINLOCK_INIT;
 
-static void __check_fromhost()
+static inline void htif_send(uint8_t dev, uint8_t cmd, int64_t data)
 {
-  uint64_t fh = fromhost;
-  if (!fh)
-    return;
-  fromhost = 0;
-
-  // this should be from the console
-  switch (FROMHOST_CMD(fh)) {
-    case 0:
-      htif_console_buf = 1 + (uint8_t)FROMHOST_DATA(fh);
-      break;
-    case 1:
-      break;
-    default:
-      break;
-  }
+    tohost = (uint64_t)dev << 56 | (uint64_t)cmd << 48 | data;
 }
 
-static void __set_tohost(uintptr_t dev, uintptr_t cmd, uintptr_t data)
+static inline void htif_recv(uint8_t *dev, uint8_t *cmd, int64_t *data)
 {
-  while (tohost)
-    __check_fromhost();
-  tohost = TOHOST_CMD(dev, cmd, data);
+    uint64_t val = fromhost;
+    *dev = val >> 56;
+    *cmd = (val >> 48) & 0xff;
+    *data = val << 16 >> 16;
 }
 
-static void do_tohost_fromhost(uintptr_t dev, uintptr_t cmd, uintptr_t data)
+static int64_t htif_get_fromhost(uint8_t dev, uint8_t cmd)
 {
-  spinlock_lock(&htif_lock);
-    __set_tohost(dev, cmd, data);
+    uint8_t rdev, rcmd;
+    int64_t data;
+    htif_recv(&rdev, &rcmd, &data);
+    return rdev == dev && rcmd == cmd ? data : -1;
+}
 
-    while (1) {
-      uint64_t fh = fromhost;
-      if (fh) {
-        if (FROMHOST_DEV(fh) == dev && FROMHOST_CMD(fh) == cmd) {
-          fromhost = 0;
-          break;
-        }
-        __check_fromhost();
-      }
+static void htif_set_tohost(uint8_t dev, uint8_t cmd, int64_t data)
+{
+    while (tohost) {
+        asm volatile ("" : : "r" (fromhost));
     }
-  spinlock_unlock(&htif_lock);
+    htif_send(dev, cmd, data);
 }
 
 int getchar()
 {
-  spinlock_lock(&htif_lock);
-    __check_fromhost();
-    int ch = htif_console_buf;
-    if (ch >= 0) {
-      htif_console_buf = -1;
-      __set_tohost(1, 0, 0);
+    int ch;
+    spinlock_lock(&htif_lock);
+    if (ch = htif_get_fromhost(1, 0) & 0xff) {
+        htif_set_tohost(1, 0, 0);
     }
-  spinlock_unlock(&htif_lock);
-
-  return ch - 1;
+    spinlock_unlock(&htif_lock);
+    return ch;
 }
 
 int putchar(int ch)
 {
-  spinlock_lock(&htif_lock);
-    __set_tohost(1, 1, ch & 0xff);
-  spinlock_unlock(&htif_lock);
-  return ch & 0xff;
+    spinlock_lock(&htif_lock);
+    htif_set_tohost(1, 1, ch & 0xff);
+    spinlock_unlock(&htif_lock);
+    return ch & 0xff;
 }
 
 void poweroff()
 {
-  while (1) {
-    fromhost = 0;
-    tohost = 1;
-  }
+    for (;;) {
+        htif_set_tohost(0, 0, 1);
+    }
 }
 
 void init() {}
