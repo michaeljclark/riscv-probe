05e078cdfd7baaa1e48239d0a60873b8610d9844
Michael Clark <mjc@sifive.com>
2018-09-13 20:36:46 +1200

    Add section start end symbols to constants.s

--- env/common/constants.s
+++ env/common/constants.s
@@ -5,3 +5,12 @@
 .equ STACK_SIZE,   1024
 .equ STACK_SHIFT,  10
 .equ CONTEXT_SIZE, (SAVE_REGS * REGBYTES)
+
+.globl _text_start
+.globl _text_end
+.globl _rodata_start
+.globl _rodata_end
+.globl _data_start
+.globl _data_end
+.globl _bss_start
+.globl _bss_end
