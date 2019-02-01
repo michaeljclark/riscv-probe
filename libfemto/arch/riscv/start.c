794635db72d192035617f711bf44e00537c5b6d6
Michael Clark <michaeljclark@mac.com>
2019-02-01 20:27:59 +1300

    Zero bss before calling main

--- libfemto/arch/riscv/start.c
+++ libfemto/arch/riscv/start.c
@@ -4,6 +4,7 @@
 #include "arch/riscv/encoding.h"
 #include "arch/riscv/machine.h"
 
+extern char _bss_start;
 extern char _bss_end;
 extern char _memory_end;
 
@@ -12,6 +13,7 @@ int main(int argc, char **argv);
 __attribute__((noreturn)) void libfemto_start_main()
 {
 	char *argv[] = { "femto", NULL };
+	memset(&_bss_start, 0, &_bss_end - &_bss_start);
 	arch_setup();
 	_malloc_addblock(&_bss_end, &_memory_end - &_bss_end);
 	exit(main(1, argv));
