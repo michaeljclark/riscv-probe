820c73e2333d5f6914812c9d1d5f3aa5717f773b
Michael Clark <michaeljclark@mac.com>
2019-02-01 20:25:28 +1300

    Shift volatile qualifier from inline onto arrays

--- libfemto/drivers/htif.c
+++ libfemto/drivers/htif.c
@@ -5,8 +5,8 @@
 #include "femto.h"
 #include "arch/riscv/atomic.h"
 
-volatile uint64_t tohost __attribute__((section(".htif")));
-volatile uint64_t fromhost __attribute__((section(".htif")));
+struct { uint32_t arr[2]; } volatile tohost __attribute__((section(".htif")));
+struct { uint32_t arr[2]; } volatile fromhost __attribute__((section(".htif")));
 
 static spinlock_t htif_lock = SPINLOCK_INIT;
 
@@ -16,16 +16,16 @@ static inline void htif_send(uint8_t dev, uint8_t cmd, int64_t data)
     union { uint32_t arr[2]; uint64_t val; } encode = {
         .val = (uint64_t)dev << 56 | (uint64_t)cmd << 48 | data
     };
-    ((volatile uint32_t *)&tohost)[0] = encode.arr[0];
-    ((volatile uint32_t *)&tohost)[1] = encode.arr[1];
+    tohost.arr[0] = encode.arr[0];
+    tohost.arr[1] = encode.arr[1];
 }
 
 static inline void htif_recv(uint8_t *dev, uint8_t *cmd, int64_t *data)
 {
     /* endian neutral decoding with ordered 32-bit reads */
     union { uint32_t arr[2]; uint64_t val; } decode;
-    decode.arr[0] = ((volatile uint32_t *)&fromhost)[0];
-    decode.arr[1] = ((volatile uint32_t *)&fromhost)[1];
+    decode.arr[0] = fromhost.arr[0];
+    decode.arr[1] = fromhost.arr[1];
     *dev = decode.val >> 56;
     *cmd = (decode.val >> 48) & 0xff;
     *data = decode.val << 16 >> 16;
@@ -43,9 +43,9 @@ static int64_t htif_get_fromhost(uint8_t dev, uint8_t cmd)
 static void htif_set_tohost(uint8_t dev, uint8_t cmd, int64_t data)
 {
     /* send data with specified device and command */
-    while (tohost) {
-        asm volatile ("" : : "r" (((volatile uint32_t *)&fromhost)[0]));
-        asm volatile ("" : : "r" (((volatile uint32_t *)&fromhost)[1]));
+    while (tohost.arr[0]) {
+        asm volatile ("" : : "r" (fromhost.arr[0]));
+        asm volatile ("" : : "r" (fromhost.arr[1]));
     }
     htif_send(dev, cmd, data);
 }
@@ -54,7 +54,7 @@ static int htif_getchar()
 {
     int ch;
     spinlock_lock(&htif_lock);
-    if (ch = htif_get_fromhost(1, 0) & 0xff) {
+    if ((ch = htif_get_fromhost(1, 0) & 0xff)) {
         htif_set_tohost(1, 0, 0);
     }
     spinlock_unlock(&htif_lock);
