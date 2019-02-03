913bccb2deb75c92c2c9a62408e6529fe402c486
Michael Clark <michaeljclark@mac.com>
2019-02-03 15:06:43 +1300

    Use macros instead of generics for ctz and clz

--- libfemto/include/stdbits.h
+++ libfemto/include/stdbits.h
@@ -6,43 +6,27 @@ extern "C" {
 
 #include <stdint.h>
 
-#if __SIZE_WIDTH__ == 32
-#define ctzl ctz32
-#define clzl clz32
-#endif
+#define clz(val) ({                         \
+    int result;                             \
+    switch(sizeof(val)) {                   \
+    case 1: result = clz8(val); break;      \
+    case 2: result = clz16(val); break;     \
+    case 4: result = clz32(val); break;     \
+    case 8: result = clz64(val); break;     \
+    }                                       \
+    result;                                 \
+})
 
-#if __SIZE_WIDTH__ == 64
-#define ctzl ctz64
-#define clzl clz64
-#endif
-
-#define clz(X) _Generic((X),                \
-                 char: clz8,                \
-                 signed char: clz8,         \
-                 unsigned char: clz8,       \
-                 short: clz16,              \
-                 unsigned short: clz16,     \
-                 int: clz32,                \
-                 unsigned int: clz32,       \
-                 long: clzl,                \
-                 unsigned long: clzl,       \
-                 long long: clz64,          \
-                 unsigned long long: clz64  \
-                 )(X)
-
-#define ctz(X) _Generic((X),                \
-                 char: ctz8,                \
-                 unsigned char: ctz8,       \
-                 signed char: ctz8,         \
-                 short: ctz16,              \
-                 unsigned short: ctz16,     \
-                 int: ctz32,                \
-                 unsigned int: ctz32,       \
-                 long: ctzl,                \
-                 unsigned long: ctzl,       \
-                 long long: ctz64,          \
-                 unsigned long long: ctz64  \
-                 )(X)
+#define ctz(val) ({                         \
+    int result;                             \
+    switch(sizeof(val)) {                   \
+    case 1: result = ctz8(val); break;      \
+    case 2: result = ctz16(val); break;     \
+    case 4: result = ctz32(val); break;     \
+    case 8: result = ctz64(val); break;     \
+    }                                       \
+    result;                                 \
+})
 
 int clz8(int8_t val);
 int clz16(int16_t val);
